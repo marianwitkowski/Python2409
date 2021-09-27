8cb2908ec33cb63e0a47a4b7001a535f43445158
kurs <kurs@DESKTOP-V3HDKPQ>
2021-09-27 15:00:53 +0200

    Kolejne skrypty

--- Dzien02/08-db.py
+++ Dzien02/08-db.py
@@ -1,5 +1,36 @@
 
 # Lączenie z bazą danych
 import psycopg2
+import datetime
 
 conn = psycopg2.connect()
+cursor = conn.cursor()
+
+# operacja na danych
+# SELECT
+sql = "SELECT * FROM public.city WHERE city_id=6"
+cursor.execute(sql)
+row = cursor.fetchone()
+print(row)
+
+print("-"*60)
+sql = "SELECT * FROM public.city WHERE city_id<10"
+cursor.execute(sql)
+rows = cursor.fetchall()
+print(rows)
+
+# INSERT
+sql = """
+
+    INSERT INTO public.city 
+            (city, country_id, last_update)
+            VALUES
+            (%s , 15, %s)
+
+"""
+data = ("Marianowo", datetime.datetime.today() )
+cursor.execute(sql, data)
+conn.commit()
+
+cursor.close()
+conn.close()
